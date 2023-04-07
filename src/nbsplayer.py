74eb2d19bfe2271b535e6cc7cad27b6721ec8aa1
Thomas Morton <sc21tam@leeds.ac.uk>
2023-04-07 18:05:32 +0100

    Refactor bot

--- src/nbsplayer.py
+++ src/nbsplayer.py
@@ -1,11 +1,12 @@
 import os
 import asyncio
+import botlog
 import random
-import paginator
 import nbswave as nbs
 import discord
 from discord import app_commands
 from discord.ext import commands
+from paginator import Paginator
 from collections import deque
 
 
@@ -15,144 +16,160 @@ class Player(commands.Cog):
         self.shuffling = False
 
     
-    async def _song_selector(self, song):
-        if not os.path.isdir(os.path.join(os.path.dirname(__file__), os.path.join("..", "cache"))):
-            os.mkdir(os.path.join(os.path.dirname(__file__), os.path.join("..", "cache")))
-        
-        # Capitalize each word in song title input.
-        title = song.split(" ")
-        song = ""
-        for word in title:
-            song += word.upper() + " "
-        song = song.strip()
+    async def _select_song(self, song):
+        cache_path = os.path.join(os.path.dirname(__file__), os.path.join("..", "cache"))
+        nbs_path = os.path.join(os.path.dirname(__file__), os.path.join("..", "nbs"))
+        sound_path = os.path.join(os.path.dirname(__file__), os.path.join("..", "sounds"))
 
-        # https://ffbinaries.com/downloads
-        if not os.path.isfile(os.path.join(os.path.dirname(__file__), os.path.join("..", "cache", "{}.mp3".format(song)))):
-            nbs_file = os.path.join(os.path.dirname(__file__), os.path.join("..", "nbs", "{}.nbs".format(song)))
-            nbs.render_audio(nbs_file, os.path.join(os.path.dirname(__file__), os.path.join("..", "cache", "{}.mp3".format(song))),
-                                format="mp3")
+        # Generate .nbs song mp3 cache folder if it does not yet exist.
+        if not os.path.isdir(cache_path):
+            os.mkdir(cache_path)
 
-        return discord.FFmpegPCMAudio(os.path.join(os.path.dirname(__file__), os.path.join("..", "cache", "{}.mp3".format(song))))
+        # Generate the mp3 file for the song if it has not already been generated.
+        cache_file = os.path.join(cache_path, "{}.mp3".format(song))
+        if not os.path.isfile(os.path.join(cache_path, "{}.mp3".format(song))):
+            nbs_file = os.path.join(nbs_path, "{}.nbs".format(song))
+
+            nbs.render_audio(song_path=nbs_file, 
+                             output_path=cache_file,
+                             custom_sound_path=sound_path,
+                             format="mp3")
+
+        return discord.FFmpegPCMAudio(cache_file)
 
 
     # Play a noteblock song from a selection of .nbs files.
     @app_commands.command(name="play", description="Play a noteblock song in your current voice channel")
     @app_commands.describe(song="Name of the noteblock song to play")
     async def play_song(self, interaction: discord.Interaction, song: str):
-        voice_state = interaction.user.voice
+        await botlog.command_used(interaction.user.name + "#" + interaction.user.discriminator,
+                                  interaction.command.name)
         
-        if voice_state is not None and not self.shuffling:
-            voice_channel = voice_state.channel
-
-            # Check if a ForumBot voice connection to the guild already exists.
-            voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
-
-            if voice_client and voice_client.is_connected():
-                voice = voice_client
-            else:
-                voice = await voice_channel.connect()
-
-            await interaction.response.defer()
-            try:
-                voice.stop()
-                audio = await self._song_selector(song)
-                voice.play(audio)
-                await interaction.followup.send("<:jukebox:1063164063590404207> Now playing: `{}`".format(song))
-            except FileNotFoundError:
-                await interaction.followup.send("I'm sorry, I don't know `{}` <:whygod:1061614468234235904>".format(song))
-                return
-
-        elif self.shuffling:
-            await interaction.response.send_message("A shuffled playlist is currently playing", ephemeral=True)
-            
-        else:
+        voice_state = interaction.user.voice
+        if voice_state is None:
             await interaction.response.send_message("You are not in a voice channel!", ephemeral=True)
+
+        if self.shuffling:
+            await interaction.response.send_message("A shuffled playlist is currently playing", ephemeral=True)
+        
+        # Check if a ForumBot voice connection to the guild already exists
+        voice_channel = voice_state.channel
+        voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
+
+        if voice_client and voice_client.is_connected():
+            voice = voice_client
+        else:
+            # Make ForumBot connect to the user's voice channel.
+            voice = await voice_channel.connect()
+
+        # Capitalize each word in song title input.
+        song = song.title()
+
+        await interaction.response.defer()
+        try:
+            voice.pause()
+            audio = await self._select_song(song)
+            voice.play(audio)
+            await interaction.followup.send("<:jukebox:1063164063590404207> Now playing: `{}`".format(song))
+        
+        except FileNotFoundError:
+            voice.resume()
+            await interaction.followup.send("I'm sorry, I don't know `{}` <:whygod:1061614468234235904>".format(song))
+            return
 
     
     # Continuosuly play random songs
     @app_commands.command(name="shuffle", description="Play random songs continuously")
     async def shuffle(self, interaction: discord.Interaction):
+        await botlog.command_used(interaction.user.name + "#" + interaction.user.discriminator,
+                                  interaction.command.name)
+        
         voice_state = interaction.user.voice
         
-        if voice_state is not None:
-            voice_channel = voice_state.channel
-
-            # Check if a ForumBot voice connection to the guild already exists.
-            voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
-
-            if voice_client and voice_client.is_connected():
-                voice = voice_client
-            else:
-                voice = await voice_channel.connect()
-            
-            song_list = os.listdir(os.path.join(os.path.dirname(__file__), os.path.join("..", "nbs")))
-            song = random.choice(song_list)[:-4]
-
-            await interaction.response.defer()
-            await interaction.followup.send("**Shuffling**")
-            voice.stop()
-            self.shuffling = True
-
-            while self.shuffling:
-                await asyncio.sleep(2.0)
-                if not voice.is_playing() and self.shuffling:
-                    try:
-                        voice.stop()
-                        audio = await self._song_selector(song)
-                        voice.play(audio)
-                        await interaction.channel.send("<:jukebox:1063164063590404207> Now playing: `{}`".format(song))
-                    except FileNotFoundError:
-                        await interaction.followup.send("I'm sorry, I don't know `{}` <:whygod:1061614468234235904>".format(song))
-
-                    song = random.choice(song_list)[:-4]
-
-            return
-        
-        else:
+        if voice_state is None:
             await interaction.response.send_message("You are not in a voice channel!", ephemeral=True)
+
+        # Check if a ForumBot voice connection to the guild already exists.
+        voice_channel = voice_state.channel
+        voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
+
+        if voice_client and voice_client.is_connected():
+            voice = voice_client
+        else:
+            # Make ForumBot connect to the user's voice channel.
+            voice = await voice_channel.connect()
+        
+        # Get the list of all song files found under ../nbs
+        song_list = os.listdir(os.path.join(os.path.dirname(__file__), os.path.join("..", "nbs")))
+        song = random.choice(song_list)[:-4]
+
+        await interaction.response.defer()
+        await interaction.followup.send("**Shuffling**")
+        voice.stop()
+        self.shuffling = True
+
+        while self.shuffling:
+            # Delay 2 seconds between songs.
+            await asyncio.sleep(2.0)
+
+            if not voice.is_playing() and self.shuffling:
+                try:
+                    voice.stop()
+                    audio = await self._select_song(song)
+                    voice.play(audio)
+                    await interaction.channel.send("<:jukebox:1063164063590404207> Now playing: `{}`".format(song))
+                except FileNotFoundError:
+                    await interaction.followup.send("I'm sorry, I don't know `{}` <:whygod:1061614468234235904>".format(song))
+
+                song = random.choice(song_list)[:-4]
+        return
 
 
     @app_commands.command(name="stop", description="Stops the current song")
     async def stop(self, interaction: discord.Interaction):
+        await botlog.command_used(interaction.user.name + "#" + interaction.user.discriminator,
+                                  interaction.command.name)
+        
         voice_state = interaction.user.voice
         
-        if voice_state is not None and self.shuffling:
-            # Check if a ForumBot voice connection to the guild already exists.
-            voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
+        if voice_state is None:
+            await interaction.response.send_message("You are not in a voice channel!", ephemeral=True)
 
-            if voice_client and voice_client.is_connected():
-                voice = voice_client
-                self.shuffling = False
-                voice.stop()
-                await interaction.response.send_message("**Stopped shuffling**")
-
-        elif voice_state is not None and not self.shuffling:
-            voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
-
-            if voice_client and voice_client.is_connected():
-                voice = voice_client
-                self.shuffling = False
-                voice.stop()
-                await interaction.response.send_message("**Stopped playing**")
-
-        elif not self.shuffling:
+        # Check if a ForumBot voice connection to the guild already exists.
+        voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
+        if not voice_client or not voice_client.is_connected():
+            return
+        
+        voice = voice_client
+        if not voice.is_playing():
             await interaction.response.send_message("Nothing to stop", ephemeral=True)
 
-        else:
-            await interaction.response.send_message("You are not in a voice channel!", ephemeral=True)
+        # Stop shuffling songs.
+        elif self.shuffling:
+            self.shuffling = False
+            voice.stop()
+            await interaction.response.send_message("**Stopped shuffling**")
+
+        # Stop the currently playing song.
+        elif not self.shuffling:
+            self.shuffling = False
+            voice.stop()
+            await interaction.response.send_message("**Stopped playing**")
 
 
     @app_commands.command(name="songs", description="Lists all playable songs")
     async def song_list(self, interaction: discord.Interaction):
+        await botlog.command_used(interaction.user.name + "#" + interaction.user.discriminator,
+                                  interaction.command.name)
+        
         embeds = []
         song_list = os.listdir(os.path.join(os.path.dirname(__file__), os.path.join("..", "nbs")))
         song_list.sort(reverse=True)
-        song_list.remove("more.txt")
 
+        # Build up a paginator with each embed containing no more than 10 songs.
         q = deque(song_list)
-
         while len(q) != 0:
-            e = discord.Embed(title="Songs", color=discord.Color.random())
+            e = discord.Embed(title="Songs", color=discord.Color.from_str("#555555"))
             e.set_author(name="NBS Player")
             e.description = ""
 
@@ -166,26 +183,29 @@ class Player(commands.Cog):
         next_button = discord.ui.Button(label="\u25ba", style=discord.ButtonStyle.primary)
         prev_button = discord.ui.Button(label="\u25c4", style=discord.ButtonStyle.primary)
         
-        await paginator.Simple(NextButton=next_button, PreviousButton=prev_button, DeleteOnTimeout=True).start(interaction, embeds)
+        await Paginator(next_button=next_button, previous_button=prev_button, delete_on_timeout=True).start(interaction, embeds)
 
 
     @app_commands.command(name="leave", description="Disconnects the bot from voice channels")
     async def disconnect(self, interaction: discord.Interaction):
-        voice_state = interaction.user.voice
+        await botlog.command_used(interaction.user.name + "#" + interaction.user.discriminator,
+                                  interaction.command.name)
         
-        if voice_state is not None:
-            # Check if a ForumBot voice connection to the guild already exists.
-            voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
-
-            if voice_client and voice_client.is_connected():
-                self.shuffling = False
-                voice = voice_client
-                voice.stop()
-                await voice.disconnect()
-                await interaction.response.send_message("Ciao!")
-
-        else:
+        voice_state = interaction.user.voice
+        if voice_state is None:
             await interaction.response.send_message("You are not in a voice channel!", ephemeral=True)
+        
+        # Check if a ForumBot voice connection to the guild already exists.
+        voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
+        if voice_client and voice_client.is_connected():
+            self.shuffling = False
+            voice = voice_client
+            voice.stop()
+            await voice.disconnect()
+            await interaction.response.send_message("Ciao!")
+        
+        else:
+            await interaction.response.send_message("I'm not connected to a voice channel!", ephemeral=True)
 
 
 async def setup(bot):
