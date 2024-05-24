062d4475d613d6c431a8d2120e08ddc3fd723633
Thomas Morton <tommeltot@icloud.com>
2024-05-24 14:35:35 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="embed-pagination",
+    version="1.0.0",
+    description="A discord.py cog that adds Minecraft note block song playback functionality to your bot.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=["nbsplayer"],
+    package_dir={'': "src"},
+    url="https://github.com/FaddyManatee/nbsplayer",
+    author="FaddyManatee",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ]
+)
